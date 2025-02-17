64e8c6f5132b9efb646902ccda97bffce361df77
terryweis <weist2088@gmail.com>
2025-02-17 00:52:34 -0600

    Project Creation

---
+++ src/main/java/com/weist/Q1_2025_software_dev_project_challenge/Q12025SoftwareDevProjectChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.weist.Q1_2025_software_dev_project_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Q12025SoftwareDevProjectChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Q12025SoftwareDevProjectChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/weist/Q1_2025_software_dev_project_challenge/Q12025SoftwareDevProjectChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weist.Q1_2025_software_dev_project_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Q12025SoftwareDevProjectChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
